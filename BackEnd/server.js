8a34bcfb4d8c940b94c49a8f530924c4a39c2b25
leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
2023-07-21 10:16:34 +0700

    nothing
    testing

--- BackEnd/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const mysql = require('mysql');
-const app = express();
-//const db = mysql.createConnection()
-
-
- app.listen(8000,()=>{
-    console.log("listening...");
- })
