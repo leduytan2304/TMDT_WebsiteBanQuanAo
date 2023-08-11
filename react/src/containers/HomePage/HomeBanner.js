c9424696c93e01915ff644d2d38554d235fadb2a
leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
2023-08-11 11:54:56 +0700

    nothing

---
+++ react/src/containers/HomePage/HomeBanner.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+
+import './HomeBanner.scss';
+import event from '../../assets/background-event.jpg'
+
+class HomeBanner extends Component {
+    render() {
+
+        return (
+            <div className='home-banner'>
+                <a href = 'https://highclub.vn/collections/sale'>
+                    <img className='home-banner-content' src = {event} />
+                </a>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeBanner);
---
+++ react/src/containers/HomePage/Section/SieuSale.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './SieuSale.scss';
+import { FormattedMessage } from 'react-intl';
+import Slider from 'react-slick';
+// Import css files:
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import SieuSalePage from '../../Customer/SieuSale/SieuSalePage';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+
+
+class SieuSale extends Component {
+
+    render() {
+        let settings = {
+            dots: false,
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000
+        };
+        return (
+            <div className='section-sieusale'>
+                <div className='sieusale-container'>
+                    <div className='section-header'>
+                        <span className='title-section title-sieusale'>Siêu Sale</span>
+                        <Link to ='/sieu-sale'>
+                            <button className='btn-section'>
+                                XEM THÊM
+                            </button>
+                        </Link>
+                    </div>
+                    <div className='sieusale-body'>
+                        <Slider {...settings}>
+                            <div className='sieusale-customize'>
+                                {/* <img src= {sieusaleImg} />  */}
+                                <div className='bg-image'>
+                                    <div className='product-discount'>
+                                        <span>-6%</span>
+                                    </div>
+                                </div>
+                                <div className='product-detail text-center'>
+                                    <div className='product-name'>Basic Tee - Brown/White </div>
+                                    <div className='product-price'>
+                                        <span>179,000₫</span>
+                                        <del>190,000₫</del>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='sieusale-customize'>
+                                {/* <img src= {sieusaleImg} /> */}
+                                <div className='bg-image'>
+                                    <div className='product-discount'>
+                                        <span>-6%</span>
+                                    </div>
+                                </div>
+                                <div className='product-detail text-center'>
+                                    <div className='product-name'>Basic Tee - Brown/White </div>
+                                    <div className='product-price'>
+                                        <span>179,000₫</span>
+                                        <del>190,000₫</del>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='sieusale-customize'>
+                                {/* <img src= {sieusaleImg} /> */}
+                                <div className='bg-image'>
+                                    <div className='product-discount'>
+                                        <span>-6%</span>
+                                    </div>
+                                </div>
+                                <div className='product-detail text-center'>
+                                    <div className='product-name'>Basic Tee - Brown/White </div>
+                                    <div className='product-price'>
+                                        <span>179,000₫</span>
+                                        <del>190,000₫</del>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='sieusale-customize'>
+                                {/* <img src= {sieusaleImg} /> */}
+                                <div className='bg-image'>
+                                    <div className='product-discount'>
+                                        <span>-6%</span>
+                                    </div>
+                                </div>
+                                <div className='product-detail text-center'>
+                                    <div className='product-name'>Basic Tee - Brown/White </div>
+                                    <div className='product-price'>
+                                        <span>179,000₫</span>
+                                        <del>190,000₫</del>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='sieusale-customize'>
+                                {/* <img src= {sieusaleImg} /> */}
+                                <div className='bg-image'>
+                                    <div className='product-discount'>
+                                        <span>-6%</span>
+                                    </div>
+                                </div>
+                                <div className='product-detail text-center'>
+                                    <div className='product-name'>Basic Tee - Brown/White </div>
+                                    <div className='product-price'>
+                                        <span>179,000₫</span>
+                                        <del>190,000₫</del>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='sieusale-customize'>
+                                {/* <img src= {sieusaleImg} /> */}
+                                <div className='bg-image'>
+                                    <div className='product-discount'>
+                                        <span>-6%</span>
+                                    </div>
+                                </div>
+                                <div className='product-detail text-center'>
+                                    <div className='product-name'>Basic Tee - Brown/White </div>
+                                    <div className='product-price'>
+                                        <span>179,000₫</span>
+                                        <del>190,000₫</del>
+                                    </div>
+                                </div>
+                            </div>
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SieuSale);
