102ea55e6d7f210f82ea68f6533abc927e25c61b
datk1912 <datk1912@gmail.com>
2023-08-15 22:28:16 +0700

    update Payment

--- react/src/containers/User/Payment/Payment.js
+++ react/src/containers/User/Payment/Payment.js
@@ -12,8 +12,31 @@ import vnpay from '../../../assets/Users/vnpay.png';
 
 import './Payment.scss';
 
+const VND = new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+  });
+
 class Cart extends Component {
-    
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedOption: 'cod', // Giá trị mặc định được chọn
+            quantityNum: ['1','1','1'],
+            unit_sum: ['149000','149000','149000'],
+            sum: '447000',
+            discount: '0',
+            price: '447000'
+        };
+    }
+
+    handleOptionChange = (event) => {
+        this.setState({
+            selectedOption: event.target.value,
+        });
+        console.log(this.state.selectedOption)
+    }
     render() {
 
         return (
@@ -31,7 +54,7 @@ class Cart extends Component {
                         }}
                     />
                     <div class="row content-info">
-                        <div class="col-8">
+                        <div class="col-9">
                             <h2> Phương thức vận chuyển</h2>
                             <form>
                                 <label class="row shipping-method" for="sm1">
@@ -68,7 +91,7 @@ class Cart extends Component {
                                         <h3>Thanh toán tiền mặt</h3>
                                     </div>
                                     <div class="col-1" align="right">
-                                        <input id="pm1" name="methud" type="radio" value="Nam" /> 
+                                        <input id="pm1" type="radio" value="cod" checked={this.state.selectedOption === 'cod'} onChange={this.handleOptionChange}/> 
                                     </div>
                                 </label>
 
@@ -80,7 +103,7 @@ class Cart extends Component {
                                         <h3>Thanh toán qua VNPAY</h3>
                                     </div>
                                     <div class="col-1" align="right">
-                                        <input id="pm2" name="methud" type="radio" value="Nam" /> 
+                                        <input id="pm2" name="methud" type="radio" value="vnpay" checked={this.state.selectedOption === 'vnpay'} onChange={this.handleOptionChange}/> 
                                     </div>
                                 </label>
                             </form>
@@ -90,96 +113,103 @@ class Cart extends Component {
                                         TRỞ VỀ
                                     </button>
                                 </NavLink>
-                                <NavLink to="#">
+                                {this.state.selectedOption === 'vnpay' ? (
+                                <NavLink to={`/vnpay`}>
                                     <button type="button" class="btn btn-danger btn-payment">
                                         THANH TOÁN
                                     </button>
                                 </NavLink>
+                                ) : (
+                                <NavLink to={`/success`}>
+                                    <button type="button" class="btn btn-danger btn-payment">
+                                        THANH TOÁN
+                                    </button>
+                                </NavLink>
+                                )}
                             </div>
                         </div>
 
                         <div class="col">
-                            <div class="bill">
-                                    <h2>Thông tin đơn hàng</h2>
-                                    <hr
-                                        style={{
-                                        color: 'black',
-                                        width: '85%',
-                                        height: '1.5px',
-                                        margin: '0 auto'
-                                        }}
-                                    />
-                                    <div class="row product-bill">                                 
-                                        <div class="col-9" align="left">
-                                            <b>Áo thun Highclub Signature</b>
-                                            <p>Nâu / M</p>
-                                        </div>
-                                        <div class="col" align="right"> 
-                                            <p> x1 </p>
-                                            <p> 149.000₫  </p>
-                                        </div>
+                        <div class="bill">
+                                <h2>Thông tin đơn hàng</h2>
+                                <hr
+                                    style={{
+                                    color: 'black',
+                                    width: '85%',
+                                    height: '1.5px',
+                                    margin: '0 auto'
+                                    }}
+                                />
+                                <div class="row product-bill">                                 
+                                    <div class="col-9" align="left">
+                                        <b>Áo thun Highclub Signature</b>
+                                        <p>Nâu / M</p>
                                     </div>
+                                    <div class="col" align="right"> 
+                                        <p> x{this.state.quantityNum[0]} </p>
+                                        <p> {VND.format(this.state.unit_sum[0])}  </p>
+                                    </div>
+                                </div>
 
-                                    <div class="row product-bill">                                 
-                                        <div class="col-9" align="left">
-                                            <b>Áo thun Highclub Signature</b>
-                                            <p>Nâu / M</p>
-                                        </div>
-                                        <div class="col" align="right"> 
-                                            <p> x1 </p>
-                                            <p> 149.000₫  </p>
-                                        </div>
+                                 <div class="row product-bill">                                 
+                                    <div class="col-9" align="left">
+                                        <b>Áo thun Highclub Signature</b>
+                                        <p>Nâu / M</p>
                                     </div>
+                                    <div class="col" align="right"> 
+                                        <p> x{this.state.quantityNum[1]} </p>
+                                        <p> {VND.format(this.state.unit_sum[1])}  </p>
+                                    </div>
+                                </div>
 
-                                    <div class="row product-bill">                                 
-                                        <div class="col-9" align="left">
-                                            <b>Áo thun Highclub Signature</b>
-                                            <p>Nâu / M</p>
-                                        </div>
-                                        <div class="col" align="right"> 
-                                            <p> x1 </p>
-                                            <p> 149.000₫  </p>
-                                        </div>
+                                <div class="row product-bill">                                 
+                                    <div class="col-9" align="left">
+                                        <b>Áo thun Highclub Signature</b>
+                                        <p>Nâu / M</p>
                                     </div>
-                                    <br />
-                                    <hr
-                                        style={{
-                                        color: 'black',
-                                        width: '85%',
-                                        height: '1.5px',
-                                        margin: '0 auto',
-                                        }}
-                                    />
+                                    <div class="col" align="right"> 
+                                        <p> x{this.state.quantityNum[2]} </p>
+                                        <p> {VND.format(this.state.unit_sum[2])}  </p>
+                                    </div>
+                                </div>
 
-                                    <div class="row sum">
-                                        <div class="col-8">
-                                            Thành tiền:
-                                        </div>
-                                        <div class="col" align="right">
-                                            <b>347.000₫</b>
-                                        </div>
-                                    </div>
+                                <br />
+                                
+                                <hr
+                                    style={{
+                                    color: 'black',
+                                    width: '85%',
+                                    height: '1.5px',
+                                    margin: '0 auto',
+                                    }}
+                                />
 
-                                    <div class="row discout">
-                                        <div class="col-8" id="dc1">
-                                            Giảm giá:
-                                        </div>
-                                        <div class="col" align="right" id="dc1">
-                                            <b>- 0₫</b>
-                                        </div>
+                                <div class="row sum">
+                                    <div class="col-8">
+                                        Thành tiền:
                                     </div>
+                                    <div class="col" align="right">
+                                        <b>{VND.format(this.state.sum)}</b>
+                                    </div>
+                                </div>
 
-                                    <div class="row final-price">
-                                        <div class="col-8">
-                                            Tổng tiền:
-                                        </div>
-                                        <div class="col" align="right" id="price">
-                                            <b>347.000₫</b>
-                                        </div>
+                                <div class="row discout">
+                                    <div class="col-8" id="dc1">
+                                        Giảm giá:
                                     </div>
-                                    <div  align="center">
-                                        
+                                    <div class="col" align="right" id="dc1">
+                                        <b>- {VND.format(this.state.discount)}</b>
                                     </div>
+                                </div>
+
+                                <div class="row final-price">
+                                    <div class="col-8">
+                                        Tổng tiền:
+                                    </div>
+                                    <div class="col" align="right" id="price">
+                                        <b>{VND.format(this.state.price)}</b>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
