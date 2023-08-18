170bf24971ccc4b3bd19acba761c936f827d5655
Minh <minh2209ak@gmail.com>
2023-08-18 16:01:13 +0700

    Update

--- react/src/services/userService.js
+++ react/src/services/userService.js
@@ -7,11 +7,6 @@ const handleLoginApi = (userEmail, userPassword) => {
     return axios.post('http://localhost:8000/api/login', {Email: userEmail, password: userPassword});
 }
 
-const personsObject = JSON.parse(JSON.parse(localStorage.getItem('persist:user')).userInfo)?.UserID;
-
-console.log(personsObject)
-
-
 const handleRegisterApi = (userEmail, userName, userPhone, userPassword) => {
     return axios.post('http://localhost:8000/api/register', {Email: userEmail, LastName: userName, Tel: userPhone, password: userPassword});
 }
