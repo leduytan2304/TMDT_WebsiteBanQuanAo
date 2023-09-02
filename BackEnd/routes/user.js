2621ca12f865ba1c710532608748cd78751d22c9
Minh <minh2209ak@gmail.com>
2023-09-02 11:15:54 +0700

    Test sort

--- BackEnd/routes/user.js
+++ BackEnd/routes/user.js
@@ -2,12 +2,9 @@ import express from "express";
 import { getUserInfo, getUserAddress, getOrderHistory, editUserInfo } from "../controllers/user.js";
 const router = express.Router()
 
-
 router.get("/profile/:userID", getUserInfo);
 router.put("/profile/edit", editUserInfo);
 router.get("/address/:userID", getUserAddress);
 router.get("/order/:userID", getOrderHistory);
-
-
 
 export default router
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,16 +1,12 @@
 import express from "express";
 import { verifyJWT } from "../controllers/auth.js";
 import { getImage,getImagePant, getImageTShirt, getProductsFilter } from "../controllers/product.js";
-
 const router = express.Router()
 
 router.get("/ao", getImageTShirt)
 router.get("/quan", getImagePant)
 router.get("/do",getImage); 
 router.get("/search",getProductsFilter); 
-
-
-
 
 
 export default router
