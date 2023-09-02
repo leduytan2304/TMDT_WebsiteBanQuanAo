8e99b77d61bb6b40c1444b27f5d0cf15ca2e7322
leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
2023-09-02 22:34:16 +0700

    cập nhật lại database

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,6 @@
 import express from "express";
 import { verifyJWT } from "../controllers/auth.js";
-<<<<<<< HEAD
 import { getImage,getImagePant, getImageTShirt } from "../controllers/product.js";
-
-=======
-import { getImage,getImagePant, getImageTShirt, getProductsFilter } from "../controllers/product.js";
->>>>>>> 2621ca12f865ba1c710532608748cd78751d22c9
 const router = express.Router()
 
 router.get("/ao", getImageTShirt)
